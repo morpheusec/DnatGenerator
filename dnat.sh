36e3dd9f204f3de5e8d759882708852f931a1882
Henry Guerra <32660595+morpheusec@users.noreply.github.com>
2021-02-10 07:22:09 -0500

    first version
    Version basada en dnat para rdp port

---
+++ dnat.sh
@@ -0,0 +1,15 @@
+# Generador de reglas DNAT para iptables GNU/LINUX
+# Usando como ejemplo DNAT para RDP port
+# DNAT rule generator for GNU / LINUX iptables
+# ./ip.sh <numero de ips>
+# ./ip.sh 100 -> 100 ips
+# Autor:@morpheusec
+#!/bin/bash
+I=0
+for OUTPUT in $(seq $1)
+do
+if [ $I -le 254 ]; then
+printf "iptables -t nat -A PREROUTING -i eno1 -p tcp --dport %d -j DNAT --to  10.10.10.%d:3389\n" $((3389+$I)) $(($I+2))
+let I++
+fi
+done
